ef013a9578b0d6ef0867908f2a24a439f624ec0b
QB3 <quentin.bertrand@inria.fr>
2022-02-02 22:30:08 -0500

    made strucutre repo

---
+++ code/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+version = 0.1
+
+DISTNAME = 'slope'
+LICENSE = 'BSD (3-clause)'
+VERSION = version
+
+setup(name='slope',
+      license=LICENSE,
+      packages=['slope']
+      )

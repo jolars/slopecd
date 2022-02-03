a3b925a5179926469c8695f7c6dc07e08b4d2229
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-02-03 16:41:27 +0100

    Add penalty class with prox for sorted l1 norm (#1)
    I arranged this as a class in which you specify the regularization
    sequence and then just use it with the coefficient vector to either
    evaluate the penalty or compute the prox.
    
    Also set up test framework with unittest.
    
    Co-authored-by: mathurinm <mathurinm@users.noreply.github.com>

---
+++ code/tests/tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+import numpy as np
+
+import slope
+
+
+class TestPenalty(unittest.TestCase):
+    def test_results(self):
+        beta = np.array([3, 2.5, 1.2, -4, 0, -0.2])
+        lam = np.array([2.7, 2, 1.7, 1.1, 0.8, 0.4])
+
+        pen = slope.SortedL1Norm(lam)
+
+        self.assertAlmostEqual(pen.evaluate(beta), 22.53)
+
+        np.testing.assert_allclose(pen.prox(beta),
+                                   np.array([1.0, 0.8, 0.1, -1.3, 0.0, -0.0]))
+
+    def test_assertions(self):
+        with self.assertRaises(ValueError):
+            slope.SortedL1Norm(np.array([0.1, 0.2]))
+        with self.assertRaises(ValueError):
+            slope.SortedL1Norm(np.array([0.2, -0.2]))
+
+
+if __name__ == '__main__':
+    unittest.main()

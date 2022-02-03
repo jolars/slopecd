85101b1af19999341cbbca48b9f978d10eed0e8b
Johan Larsson <johanlarsson@outlook.com>
2022-02-03 11:52:55 +0100

    Drop stray line of code

--- code/tests/tests.py
+++ code/tests/tests.py
@@ -10,8 +10,6 @@ class TestPenalty(unittest.TestCase):
         beta = np.array([3, 2.5, 1.2, -4, 0, -0.2])
         lam = np.array([2.7, 2, 1.7, 1.1, 0.8, 0.4])
 
-        np.sum(np.sort(np.abs(beta))[::-1] * lam)
-
         pen = slope.SortedL1Norm(lam)
 
         self.assertAlmostEqual(pen.evaluate(beta), 22.53)

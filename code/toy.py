15278f1bda9a2e83984c2286b08942a0d16397ec
mathurinm <mathurin.massias@gmail.com>
2022-02-08 17:48:28 +0100

    alternate 1prox grad step with n_cd cd epcohs with freezed order

--- code/toy.py
+++ code/toy.py
@@ -1,42 +1,43 @@
 from scipy import stats
+from numba import njit
 import numpy as np
 from benchopt.datasets import make_correlated_data
 from numpy.linalg import norm
+import matplotlib.pyplot as plt
 
-from sklearn.isotonic import isotonic_regression
-
-
-def dual_norm_slope(X, theta, alphas):
-    """Dual slope norm of X.T @ theta"""
-    Xtheta = np.sort(np.abs(X.T @ theta))[::-1]
-    taus = 1 / np.cumsum(alphas)
-    return np.max(np.cumsum(Xtheta) * taus)
+from slope.utils import ST, dual_norm_slope, prox_slope
 
 
-def prox_slope(w, alphas):
-    w_abs = np.abs(w)
-    idx = np.argsort(w_abs)[::-1]
-    w_abs = w_abs[idx]
-    # projection onto Km+
-    w_abs = isotonic_regression(w_abs - alphas, y_min=0, increasing=False)
+@njit
+def do_cd_epochs(n_cd, w, X, R, alphas, lc):
+    n_samples = X.shape[0]
+    for _ in range(n_cd):
+        # do CD epochs pretending coefs order is fixed
+        order = np.argsort(np.abs(w))[::-1]
+        for idx, j in enumerate(order):  # update from big to small
+            old = w[j]
+            w[j] = ST(w[j] + X[:, j] @ R / (lc[j] * n_samples),
+                      alphas[idx] / lc[j])
+            if w[j] != old:
+                R += (old - w[j]) * X[:, j]
 
-    # undo the sorting
-    inv_idx = np.zeros_like(idx)
-    inv_idx[idx] = np.arange(len(w))
 
-    return np.sign(w) * w_abs[inv_idx]
-
-
-def prox_grad(X, y, alphas, max_iter=100, tol=1e-10, verbose=True):
+def prox_grad(X, y, alphas, max_iter=100, tol=1e-10, n_cd=0, verbose=True):
     n_samples, n_features = X.shape
     R = y.copy()
     w = np.zeros(n_features)
 
     L = norm(X, ord=2) ** 2 / n_samples
-    E = []
+    lc = norm(X, axis=0) ** 2 / n_samples
+    E, gaps = [], []
+    E.append(norm(y)**2 / (2 * n_samples))
+    gaps.append(E[0])
     for t in range(max_iter):
         w = prox_slope(w + (X.T @ R) / (L * n_samples), alphas / L)
         R[:] = y - X @ w
+
+        do_cd_epochs(n_cd, w, X, R, alphas, lc)
+
         theta = R / n_samples
         theta /= max(1, dual_norm_slope(X, theta, alphas))
         dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / \
@@ -45,20 +46,34 @@ def prox_grad(X, y, alphas, max_iter=100, tol=1e-10, verbose=True):
             np.sum(alphas * np.sort(np.abs(w))[::-1])
         E.append(primal)
         gap = primal - dual
+        gaps.append(gap)
         if verbose:
             print(f"Iter: {t + 1}, loss: {primal}, gap: {gap:.2e}")
         if gap < tol:
             break
-    return w, E, theta
+    return w, E, gaps, theta
 
 
 X, y, _ = make_correlated_data(n_samples=100, n_features=40, random_state=0)
 randnorm = stats.norm(loc=0, scale=1)
 q = 0.5
 
-alphas = 1 * \
-    randnorm.ppf(1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
-# alphas = np.full(X.shape[1], np.max(np.abs(X.T @ y)) / len(y) / 4)
+alphas_seq = randnorm.ppf(
+    1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
 
-w, E, theta = prox_grad(X, y, alphas, max_iter=1000)
-R = y - X @ w
+# alphas_seq = np.ones(X.shape[1])
+
+alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
+
+alphas = alpha_max * alphas_seq / 500
+plt.close('all')
+
+for n_cd in [0, 1, 5, 10]:
+    w, E, gaps, theta = prox_grad(
+        X, y, alphas, max_iter=1000 // (n_cd + 1), n_cd=n_cd, verbose=1)
+    print(gaps[0])
+
+    plt.semilogy(np.arange(len(E)) * (1 + n_cd), gaps,
+                 label=f'n_cd = {n_cd}')
+plt.legend()
+plt.show(block=False)

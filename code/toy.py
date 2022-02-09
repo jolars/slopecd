8fbb6465df304c133736913359374813874cd735
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-02-09 10:34:18 +0100

    Strategy on clusters block CD

--- code/toy.py
+++ code/toy.py
@@ -1,3 +1,4 @@
+from cmath import isclose
 from scipy import stats
 from numba import njit
 import numpy as np
@@ -6,6 +7,25 @@ from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
 from slope.utils import ST, dual_norm_slope, prox_slope
+
+
+def get_clusters(w):
+    abs_w = np.abs(w)
+    order = np.argsort(abs_w)[::-1]
+    clusters = []
+    current_cluster = [order[0]]
+    for j in range(len(w) - 1):
+        if len(current_cluster) == 0:
+            current_cluster.append(order[j])
+        if np.isclose(abs_w[order[j]], abs_w[order[j+1]]):
+            current_cluster.append(order[j+1])
+        else:
+            clusters.append(current_cluster)
+            current_cluster = []
+
+    if len(current_cluster) != 0:
+        clusters.append(current_cluster)
+    return clusters
 
 
 @njit
@@ -54,6 +74,57 @@ def prox_grad(X, y, alphas, max_iter=100, tol=1e-10, n_cd=0, verbose=True):
     return w, E, gaps, theta
 
 
+def hybrid_strategy(X, y, alphas, max_iter=100, tol=1e-10, verbose=True):
+    n_samples, n_features = X.shape
+    R = y.copy()
+    w = np.zeros(n_features)
+    clusters = [np.arange(0, n_features)]
+
+    L = norm(X, ord=2) ** 2 / n_samples
+    lc = norm(X, axis=0) ** 2 / n_samples
+    E, gaps = [], []
+    E.append(norm(y)**2 / (2 * n_samples))
+    gaps.append(E[0])
+
+    for t in range(max_iter):
+        low = 0
+        high = 0
+        if t % 2 == 0:
+            w = prox_slope(
+                w + (X.T @ R) / (L * n_samples), alphas / L)
+            R[:] = y - X @ w
+        else:
+            for b in clusters:
+                high += len(b)
+                if len(b) == 1:
+                    # if update a single cd then use local Lipschitz constant
+                    w[b] = prox_slope(
+                        w[b] + (X[:, b].T @ R) / (
+                            lc[b] * n_samples), alphas[low:high] / lc[b])
+                else:
+                    w[b] = prox_slope(
+                        w[b] + (X[:, b].T @ R) / (
+                            L * n_samples), alphas[low:high] / L)
+                R[:] = y - X @ w
+                low = high
+        theta = R / n_samples
+        theta /= max(1, dual_norm_slope(X, theta, alphas))
+        dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / \
+            (2 * n_samples)
+        primal = norm(R) ** 2 / (2 * n_samples) + \
+            np.sum(alphas * np.sort(np.abs(w))[::-1])
+        E.append(primal)
+        gap = primal - dual
+        gaps.append(gap)
+        if verbose:
+            print(f"Iter: {t + 1}, loss: {primal}, gap: {gap:.2e}")
+        if gap < tol:
+            break
+        # Using the separability property on clusters
+        clusters = get_clusters(w)
+    return w, E, gaps, theta
+
+
 X, y, _ = make_correlated_data(n_samples=100, n_features=40, random_state=0)
 randnorm = stats.norm(loc=0, scale=1)
 q = 0.5
@@ -75,5 +146,9 @@ for n_cd in [0, 1, 5, 10]:
 
     plt.semilogy(np.arange(len(E)) * (1 + n_cd), gaps,
                  label=f'n_cd = {n_cd}')
+w, E, gaps, theta = hybrid_strategy(
+        X, y, alphas, max_iter=1000, verbose=1)
+plt.semilogy(np.arange(len(E)), gaps,
+             label='Hybrid strategy')
 plt.legend()
 plt.show(block=False)

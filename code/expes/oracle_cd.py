ef0bc62173b05fdc537aa13fbfc8c3305539bf43
mathurinm <mathurinm@users.noreply.github.com>
2022-02-11 08:36:06 +0100

    Add hybrid and oracle strategies (#3)
    Add hybrid PGD/CD and oracle strategies. The hybrid strategy is currently not quite working.

---
+++ code/expes/oracle_cd.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from benchopt.datasets import make_correlated_data
+from scipy import stats
+
+from slope.solvers import oracle_cd, prox_grad
+from slope.utils import dual_norm_slope
+
+X, y, _ = make_correlated_data(n_samples=100, n_features=40, random_state=0)
+randnorm = stats.norm(loc=0, scale=1)
+q = 0.5
+
+alphas_seq = randnorm.ppf(1 - np.arange(1, X.shape[1] + 1) * q /
+                          (2 * X.shape[1]))
+
+# make it infinity norm:
+# alphas_seq[1:] = 0
+
+alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
+
+alphas = alpha_max * alphas_seq / 5
+plt.close('all')
+
+w, E, gaps, theta = prox_grad(X,
+                              y,
+                              alphas,
+                              max_iter=1000,
+                              n_cd=0,
+                              verbose=False)
+
+w_oracle, E_oracle, gaps_oracle = oracle_cd(X, y, alphas, 1000, tol=1e-10)
+
+E_min = min(np.min(E), np.min(E_oracle))
+
+plt.semilogy(gaps, label="PGD")
+plt.semilogy(gaps_oracle, label="oracle CD")
+plt.legend()
+plt.show(block=False)
---
+++ code/slope/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numba import njit
+
+from sklearn.isotonic import isotonic_regression
+
+
+@njit
+def ST(x, u):
+    if x > u:
+        return x - u
+    elif x < -u:
+        return x + u
+    else:
+        return 0
+
+
+def dual_norm_slope(X, theta, alphas):
+    """Dual slope norm of X.T @ theta"""
+    Xtheta = np.sort(np.abs(X.T @ theta))[::-1]
+    taus = 1 / np.cumsum(alphas)
+    return np.max(np.cumsum(Xtheta) * taus)
+
+
+def prox_slope(w, alphas):
+    w_abs = np.abs(w)
+    idx = np.argsort(w_abs)[::-1]
+    w_abs = w_abs[idx]
+    # projection onto Km+
+    w_abs = isotonic_regression(w_abs - alphas, y_min=0, increasing=False)
+
+    # undo the sorting
+    inv_idx = np.zeros_like(idx)
+    inv_idx[idx] = np.arange(len(w))
+
+    return np.sign(w) * w_abs[inv_idx]

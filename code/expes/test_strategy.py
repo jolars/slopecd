5b5388d22a38b212071091ac6320649c3a3e5c28
Johan Larsson <johanlarsson@outlook.com>
2022-03-17 12:17:36 +0100

    Add new CD solver to strategy tests

--- code/expes/test_strategy.py
+++ code/expes/test_strategy.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from benchopt.datasets import make_correlated_data
 from slope.utils import dual_norm_slope
 from slope.solvers import prox_grad, oracle_cd, hybrid_cd
+from slope.solvers.proxsplit_cd import proxsplit_cd
 import time
 
 X, y, _ = make_correlated_data(n_samples=100, n_features=1000, random_state=0)
@@ -20,25 +21,29 @@ alphas = alpha_max * alphas_seq / 5
 plt.close('all')
 
 max_iter = 1000
-tol = 1e-12
+tol = 1e-10
 
 
 start_cd = time.time()
 beta_cd, primals_cd, gaps_cd = hybrid_cd(
-    X, y, alphas, max_iter=1000, verbose=True, tol=tol)
+    X, y, alphas, max_iter=max_iter, verbose=True, tol=tol)
 time_cd = time.time() - start_cd
 start_pgd = time.time()
 beta_star, primals_star, gaps_star, theta_star = prox_grad(
-    X, y, alphas, max_iter=10000, n_cd=0, verbose=True, tol=tol,
+    X, y, alphas, max_iter=max_iter, n_cd=0, verbose=True, tol=tol,
 )
 time_pgd = time.time() - start_pgd
 
 beta_oracle, primals_oracle, gaps_oracle = oracle_cd(
-    X, y, alphas, max_iter=1000, verbose=True, tol=tol,
+    X, y, alphas, max_iter=max_iter, verbose=True, tol=tol,
+)
+beta_cdsplit, primals_cdsplit, gaps_cdsplit, theta_cdsplit = proxsplit_cd(
+    X, y, alphas, max_iter=max_iter, verbose=True, tol=tol, split_freq = 1
 )
 plt.semilogy(np.arange(len(gaps_cd)), gaps_cd, label='cd')
 plt.semilogy(np.arange(len(gaps_star)), gaps_star, label='pgd')
 plt.semilogy(np.arange(len(gaps_oracle)), gaps_oracle, label='oracle')
+plt.semilogy(np.arange(len(gaps_cdsplit)), gaps_cdsplit, label='cd_proxsplit')
 
 plt.legend()
-plt.show()
+plt.show(block=False)

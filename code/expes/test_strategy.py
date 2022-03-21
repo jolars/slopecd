457876ad210d20e057b7cf631854b9240a4fbab8
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-03-21 21:21:59 +0100

    Collect timings from inside solvers (#17)
    * Collect timings from inside solvers
    
    * Update return assignment from other experiments
    
    * time > times, rm theta in prox_grad
    
    * rm two thetas prox grad
    
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>

--- code/expes/test_strategy.py
+++ code/expes/test_strategy.py
@@ -35,39 +35,52 @@ max_epochs = 10000
 tol = 1e-10
 
 
-start_cd = time.time()
-beta_cd, primals_cd, gaps_cd = hybrid_cd(
+beta_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
     X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol)
-time_cd = time.time() - start_cd
-start_pgd = time.time()
-beta_star, primals_star, gaps_star, theta_star = prox_grad(
+beta_pgd, primals_pgd, gaps_pgd, time_pgd = prox_grad(
+    X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol, fista=True
+)
+beta_oracle, primals_oracle, gaps_oracle, time_oracle = oracle_cd(
     X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol,
 )
-time_pgd = time.time() - start_pgd
-
-beta_oracle, primals_oracle, gaps_oracle = oracle_cd(
-    X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol,
-)
-beta_cdsplit, primals_cdsplit, gaps_cdsplit, theta_cdsplit = proxsplit_cd(
+beta_cdsplit, primals_cdsplit, gaps_cdsplit, theta_cdsplit, time_cdsplit = proxsplit_cd(
     X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol, split_freq=1
 )
 
+# Duality gap vs epoch
+plt.clf()
 
 plt.semilogy(np.arange(len(gaps_cd)), gaps_cd, label='cd')
-plt.semilogy(np.arange(len(gaps_star)), gaps_star, label='pgd')
+plt.semilogy(np.arange(len(gaps_pgd)), gaps_pgd, label='pgd')
 plt.semilogy(np.arange(len(gaps_oracle)), gaps_oracle, label='oracle')
 plt.semilogy(np.arange(len(gaps_cdsplit)), gaps_cdsplit, label='cd_proxsplit')
 
 plt.legend()
 plt.title(dataset)
-plt.show()
+plt.show(block=False)
 
+# Duality gap vs epoch for hybrid vs oracle
+plt.clf()
 
 plt.semilogy(
-    np.arange(len(gaps_cd)), primals_cd - primals_star[-1], label='cd')
+    np.arange(len(gaps_cd)), primals_cd - primals_pgd[-1], label='cd')
 plt.semilogy(
-    np.arange(len(gaps_oracle)), primals_oracle - primals_star[-1],
+    np.arange(len(gaps_oracle)), primals_oracle - primals_pgd[-1],
     label='oracle')
 
 plt.legend()
-plt.show()
+plt.show(block=False)
+
+# Time vs. duality gap
+plt.clf()
+
+plt.semilogy(time_cd, gaps_cd, label='cd')
+plt.semilogy(time_pgd, gaps_pgd, label='pgd')
+plt.semilogy(time_oracle, gaps_oracle, label='oracle')
+plt.semilogy(time_cdsplit, gaps_cdsplit, label='cd_proxsplit')
+
+plt.ylabel("duality gap")
+plt.xlabel("Time (s)")
+plt.legend()
+plt.title(dataset)
+plt.show(block=False)

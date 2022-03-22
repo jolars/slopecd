6b1b8c1f505e6ad4ae98946ec4b33dbb667a7ff8
mathurinm <mathurin.massias@gmail.com>
2022-03-22 13:20:37 +0100

    add pgd line search to script

--- code/expes/ista_vs_fista.py
+++ code/expes/ista_vs_fista.py
@@ -37,8 +37,12 @@ plt.figure()
 max_epochs = 20_000
 tol = 1e-8
 
-w_ista, E_ista, gaps_ista, times_ista = prox_grad(
+_, E_ista, gaps_ista, times_ista = prox_grad(
     X, y, lambdas, fista=False, verbose=True, gap_freq=10,
+    max_epochs=max_epochs, tol=tol)
+
+_, E_ista_ls, gaps_ista_ls, times_ista_ls = prox_grad(
+    X, y, lambdas, fista=False, line_search=True, verbose=True, gap_freq=10,
     max_epochs=max_epochs, tol=tol)
 
 _, E_anderson, gaps_anderson, times_anderson = prox_grad(
@@ -49,7 +53,7 @@ _, E_fista_ls, gaps_fista_ls, _ = prox_grad(
     X, y, lambdas, fista=True, verbose=True, gap_freq=10,
     max_epochs=max_epochs, line_search=True, tol=tol)
 
-w_fista, E_fista, gaps_fista, times_fista = prox_grad(
+_, E_fista, gaps_fista, times_fista = prox_grad(
     X, y, lambdas, fista=True, verbose=True, gap_freq=10,
     max_epochs=max_epochs, tol=tol)
 
@@ -62,10 +66,12 @@ plt.semilogy(E_ista - p_star, c=cm(0), label='primal subopt PGD')
 plt.semilogy(E_fista - p_star, c=cm(1), label='primal subopt APGD')
 plt.semilogy(E_fista_ls - p_star, c=cm(2), label='primal subopt APGD+LS')
 plt.semilogy(E_anderson - p_star, c=cm(3), label='primal subopt PGD Anderson')
+plt.semilogy(E_ista_ls - p_star, c=cm(4), label='primal subopt PGD+LS')
 
 plt.semilogy(gaps_ista, c=cm(0), linestyle='--', label='gap PGD')
 plt.semilogy(gaps_fista, c=cm(1), linestyle='--', label='gap APGD')
 plt.semilogy(gaps_fista_ls, c=cm(2), linestyle='--', label='gap APGD+LS')
 plt.semilogy(gaps_anderson, c=cm(3), linestyle='--', label='gap PGD Anderson')
+plt.semilogy(gaps_ista_ls, c=cm(4), linestyle='--', label='gap PGD+LS')
 plt.legend()
 plt.show(block=False)

fix: fix computation of lambda_max

code/expes/intercept.py
@@ -1,64 +1,77 @@
-import numpy as np
-from scipy import stats
-from scipy import sparse
 import matplotlib.pyplot as plt
+import numpy as np
 from benchopt.datasets import make_correlated_data
-from slope.utils import dual_norm_slope
-from slope.solvers import prox_grad
-from slope.solvers import hybrid_cd
-# from slope.solvers import oracle_cd
-
 from libsvmdata import fetch_libsvm
+from numpy.linalg import norm
+from scipy import sparse, stats
 
-dataset = "real-sim"
+from slope.solvers import hybrid_cd, oracle_cd, prox_grad
+from slope.utils import dual_norm_slope
+
+dataset = "simulated"
 if dataset == "simulated":
     X, y, _ = make_correlated_data(
-        n_samples=100, n_features=10, random_state=0, rho=0.9)
-    # X = csc_matrix(X)
+        n_samples=100, n_features=10, random_state=0, rho=0.9
+    )
 else:
     X, y = fetch_libsvm(dataset)
 
-# X = X.toarray()
-# X1 = np.hstack((np.ones((X.shape[0], 1)), X))
-
-# np.linalg.norm(X, 2)
-# np.linalg.norm(X1, 2)
+n_samples, n_features = X.shape
 
 randnorm = stats.norm(loc=0, scale=1)
 q = 0.1
 
-alphas_seq = randnorm.ppf(
-    1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
+fit_intercept = True
 
+alphas_seq = randnorm.ppf(1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
 
-alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
+alpha_max = dual_norm_slope(X, (y - np.mean(y) * fit_intercept) / len(y), alphas_seq)
 
-alphas = alpha_max * alphas_seq / 5
+alphas = alpha_max * alphas_seq / 100
 
 max_epochs = 10000
 tol = 1e-6
 
-fit_intercept=True
-
 beta_cd, intercept_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
-    X, y, alphas, fit_intercept=fit_intercept, max_epochs=max_epochs, verbose=True, tol=tol)
-beta_pgd, intercept_pgd, primals_pgd, gaps_pgd, time_pgd = prox_grad(
-    X, y, alphas, fit_intercept=fit_intercept, max_epochs=max_epochs, verbose=True, tol=tol, fista=False
+    X,
+    y,
+    alphas,
+    fit_intercept=fit_intercept,
+    max_epochs=max_epochs,
+    verbose=True,
+    tol=tol,
 )
-# beta_oracle, intercept_oracle, primals_oracle, gaps_oracle, time_oracle = oracle_cd(
-#     X, y, alphas, max_epochs=max_epochs, verbose=True, tol=tol,
-# )
+beta_pgd, intercept_pgd, primals_pgd, gaps_pgd, time_pgd = prox_grad(
+    X,
+    y,
+    alphas,
+    fit_intercept=fit_intercept,
+    max_epochs=max_epochs,
+    verbose=True,
+    tol=tol,
+    fista=False,
+)
+beta_oracle, intercept_oracle, primals_oracle, gaps_oracle, time_oracle = oracle_cd(
+    X,
+    y,
+    alphas,
+    fit_intercept=fit_intercept,
+    max_epochs=max_epochs,
+    verbose=True,
+    tol=tol,
+)
 
 # Duality gap vs epoch
 plt.clf()
 
-plt.semilogy(primals_cd, label='cd')
-plt.semilogy(primals_pgd, label='pgd')
-# plt.semilogy(gaps_cd, label='cd')
-# plt.semilogy(gaps_pgd, label='pgd')
-# plt.semilogy(np.arange(len(gaps_oracle)), gaps_oracle, label='oracle')
+primal_star = np.min([np.min(primals_cd), np.min(primals_pgd), np.min(primals_oracle)])
 
+plt.semilogy(primals_cd - primal_star, label="cd")
+plt.semilogy(primals_pgd - primal_star, label="pgd")
+plt.semilogy(primals_oracle - primal_star, label="oracle")
+
+plt.ylabel("primal suboptimality")
+plt.xlabel("epoch")
 plt.legend()
 plt.title(dataset)
 plt.show(block=False)
-

code/expes/test_strategy.py
@@ -18,6 +18,8 @@ if dataset == "simulated":
 else:
     X, y = fetch_libsvm(dataset)
 
+fit_intercept=True
+
 randnorm = stats.norm(loc=0, scale=1)
 q = 0.5
 
@@ -25,7 +27,7 @@ alphas_seq = randnorm.ppf(
     1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
 
 
-alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
+alpha_max = dual_norm_slope(X, (y - fit_intercept*np.mean(y)) / len(y), alphas_seq)
 
 alphas = alpha_max * alphas_seq / 5
 plt.close('all')

code/slope/solvers/hybrid.py
@@ -100,6 +100,7 @@ def block_cd_epoch_sparse(
 
     return n_c
 
+
 @njit
 def compute_block_scalar_sparse(
         X_data, X_indices, X_indptr, v, cluster, n_samples):
@@ -191,7 +192,7 @@ def hybrid_cd(
                     n_c,
                     cluster_updates,
                 )
-        
+
             if fit_intercept:
                 intercept_update = np.sum(R) / n_samples
                 R -= intercept_update

code/slope/solvers/pgd.py
@@ -83,10 +83,11 @@ def prox_grad(
                     c = coefs / coefs.sum()
                     w_acc = np.sum(last_K_w[:-1] * c[:, None],
                                    axis=0)
-                    p_obj = norm(y - X @ w_new) ** 2 / (2 * n_samples) + \
+                    p_obj = norm(y - X @ w_new - intercept) ** 2 / (2 * n_samples) + \
                         np.sum(alphas * np.sort(np.abs(w_new))[::-1])
-                    p_obj_acc = norm(y - X @ w_acc) ** 2 / (2 * n_samples) + \
-                        np.sum(alphas * np.sort(np.abs(w_acc))[::-1])
+                    p_obj_acc = norm(y - X @ w_acc - intercept) ** 2 / (
+                        2 * n_samples
+                    ) + np.sum(alphas * np.sort(np.abs(w_acc))[::-1])
                     if p_obj_acc < p_obj:
                         w_new = w_acc
                 except np.linalg.LinAlgError:

code/tests/tests.py
@@ -40,17 +40,29 @@ class TestPGDSolvers(unittest.TestCase):
         alphas_seq = randnorm.ppf(
             1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1])
         )
-        alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
-
-        alphas = alpha_max * alphas_seq / 50
-
         for fista in [False, True]:
-            tol = 1e-10
-            w, E, gaps, _ = prox_grad(
-                X, y, alphas, fista=fista, max_epochs=15_000, gap_freq=10, verbose=False
-            )
-            with self.subTest():
-                self.assertGreater(tol, gaps[-1])
+            for fit_intercept in [False, True]:
+                alpha_max = dual_norm_slope(
+                    X,
+                    (y - np.mean(y)*fit_intercept) / len(y),
+                    alphas_seq
+                )
+
+                alphas = alpha_max * alphas_seq / 50
+
+                tol = 1e-10
+                w, intercept, E, gaps, _ = prox_grad(
+                    X,
+                    y,
+                    alphas,
+                    fista=fista,
+                    fit_intercept=fit_intercept,
+                    max_epochs=25_000,
+                    gap_freq=10,
+                    verbose=False,
+                )
+                with self.subTest():
+                    self.assertGreater(tol, gaps[-1])
 
 
 class TestClusterUpdates(unittest.TestCase):
7d844df0e638337b750828af8fed6881560913cf
Johan Larsson <johanlarsson@outlook.com>
2022-06-01 15:47:30 +0200

    chore: clean up experiment and line search code
    * Also improve BB setting with safeguarding
    * Remove some of the line search strategies

--- code/expes/ista_vs_fista.py
+++ code/expes/ista_vs_fista.py
@@ -1,19 +1,23 @@
-import numpy as np
-from matplotlib.cm import get_cmap
 import matplotlib.pyplot as plt
-from scipy import stats
-from libsvmdata import fetch_libsvm
+import numpy as np
 from benchopt.datasets.simulated import make_correlated_data
+from libsvmdata import fetch_libsvm
+from matplotlib.cm import get_cmap
+from scipy import stats
 
 from slope.solvers import prox_grad
 from slope.utils import dual_norm_slope
-cm = get_cmap('tab10')
+
+cm = get_cmap("tab10")
 
 n = 100
 p = 2000
 rho = 0.5
 
-dataset = "simulated"
+reg = 0.01
+q = 0.1
+
+dataset = "rcv1.binary"
 if dataset == "simulated":
     X, y, _ = make_correlated_data(n_samples=n, n_features=p, random_state=0, rho=rho)
 else:
@@ -22,74 +26,91 @@ else:
 y = y - np.mean(y)
 
 randnorm = stats.norm(loc=0, scale=1)
-q = 0.5
-alphas_seq = randnorm.ppf(
-    1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
-
+alphas_seq = randnorm.ppf(1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
 
 alpha_max = dual_norm_slope(X, y / len(y), alphas_seq)
 
-lambdas = alpha_max * alphas_seq / 50
+lambdas = alpha_max * alphas_seq * reg
 
 
-max_epochs = 20_000
-tol = 1e-7
+max_epochs = 200
+tol = 1e-4
+gap_freq = 10
 
 _, E_ista, gaps_ista, times_ista = prox_grad(
-    X, y, lambdas, verbose=True, gap_freq=10,
-    max_epochs=max_epochs, tol=tol)
+    X, y, lambdas, verbose=True, gap_freq=10, max_epochs=max_epochs, tol=tol
+)
 
 _, E_ista_lsbb, gaps_ista_lsbb, times_ista_lsbb = prox_grad(
-    X, y, lambdas, line_search=True, barzilai=True, verbose=True, gap_freq=10,
-    max_epochs=max_epochs, tol=tol)
+    X,
+    y,
+    lambdas,
+    bb_step_size=True,
+    line_search=True,
+    verbose=True,
+    gap_freq=10,
+    max_epochs=max_epochs,
+    tol=tol,
+)
 
 _, E_ista_ls, gaps_ista_ls, times_ista_ls = prox_grad(
-    X, y, lambdas, line_search=True, verbose=True, gap_freq=10,
-    ls_type=0,
-    max_epochs=max_epochs, tol=tol)
+    X,
+    y,
+    lambdas,
+    line_search=True,
+    verbose=True,
+    gap_freq=gap_freq,
+    max_epochs=max_epochs,
+    tol=tol,
+)
 
-_, E_ista_ls1, gaps_ista_ls1, times_ista_ls1 = prox_grad(
-    X, y, lambdas, line_search=True, verbose=True, gap_freq=10,
-    ls_type=1,
-    max_epochs=max_epochs, tol=tol)
+_, E_anderson, gaps_anderson, times_anderson = prox_grad(
+    X,
+    y,
+    lambdas,
+    acceleration="anderson",
+    verbose=True,
+    gap_freq=gap_freq,
+    max_epochs=max_epochs,
+    tol=tol,
+)
 
-_, E_ista_ls2, gaps_ista_ls2, times_ista_ls2 = prox_grad(
-    X, y, lambdas, line_search=True, verbose=True, gap_freq=10,
-    ls_type=2,
-    max_epochs=max_epochs, tol=tol)
+_, E_fista, gaps_fista, times_fista = prox_grad(
+    X,
+    y,
+    lambdas,
+    acceleration="fista",
+    verbose=True,
+    gap_freq=gap_freq,
+    max_epochs=max_epochs,
+    tol=tol,
+)
 
-_, E_ista_ls3, gaps_ista_ls3, times_ista_ls3 = prox_grad(
-    X, y, lambdas, line_search=True, barzilai=True, verbose=True, gap_freq=10,
-    ls_type=3,
-    max_epochs=max_epochs, tol=tol)
+_, E_fista_ls, gaps_fista_ls, times_fista_ls = prox_grad(
+    X,
+    y,
+    lambdas,
+    acceleration="fista",
+    verbose=True,
+    gap_freq=gap_freq,
+    max_epochs=max_epochs,
+    line_search=True,
+    tol=tol,
+)
 
-# _, E_anderson, gaps_anderson, times_anderson = prox_grad(
-#     X, y, lambdas, fista=False, anderson=True, verbose=True, gap_freq=10,
-#     max_epochs=max_epochs, tol=tol)
-
-# _, E_fista_ls, gaps_fista_ls, _ = prox_grad(
-#     X, y, lambdas, fista=True, verbose=True, gap_freq=10,
-#     max_epochs=max_epochs, line_search=True, tol=tol)
-
-# _, E_fista, gaps_fista, times_fista = prox_grad(
-#     X, y, lambdas, fista=True, verbose=True, gap_freq=10,
-#     max_epochs=max_epochs, tol=tol)
-
-
-# plt.close('all')
-# plt.figure()
 plt.clf()
-# p_star = min(np.min(E_ista), np.min(E_fista), np.min(E_fista_ls))
-p_star = min(np.min(E_ista), np.min(E_ista_ls), np.min(E_ista_lsbb))
-plt.semilogy(times_ista, E_ista - p_star, c=cm(0), label='primal subopt PGD')
-# plt.semilogy(E_fista - p_star, c=cm(1), label='primal subopt APGD')
-# plt.semilogy(E_fista_ls - p_star, c=cm(2), label='primal subopt APGD+LS')
-# plt.semilogy(E_anderson - p_star, c=cm(3), label='primal subopt PGD Anderson')
-plt.semilogy(times_ista_ls, E_ista_ls - p_star, c=cm(1), label='primal subopt PGD+LS')
-plt.semilogy(times_ista_ls1, E_ista_ls1 - p_star, c=cm(2), label='primal subopt PGD+LS1')
-plt.semilogy(times_ista_ls2, E_ista_ls2 - p_star, c=cm(3), label='primal subopt PGD+LS2')
-plt.semilogy(times_ista_ls3, E_ista_ls3 - p_star, c=cm(4), label='primal subopt PGD+LS3')
-plt.semilogy(times_ista_lsbb, E_ista_lsbb - p_star, c=cm(5), label='primal subopt PGD+LSBB')
+
+p_star = min(np.min(E_ista), np.min(E_fista), np.min(E_fista_ls), np.min(E_ista_lsbb))
+plt.semilogy(times_ista, E_ista - p_star, c=cm(0), label="primal subopt PGD")
+plt.semilogy(times_ista_ls, E_ista_ls - p_star, c=cm(1), label="primal subopt PGD+LS")
+plt.semilogy(
+    times_ista_lsbb, E_ista_lsbb - p_star, c=cm(5), label="primal subopt PGD+LS+BB"
+)
+plt.semilogy(
+    times_anderson, E_anderson - p_star, c=cm(6), label="primal subopt PGD Anderson"
+)
+plt.semilogy(times_fista, E_fista - p_star, c=cm(7), label="primal subopt FISTA")
+plt.semilogy(times_fista_ls, E_fista_ls - p_star, c=cm(8), label="primal subopt FISTA+LS")
 
 # plt.semilogy(times_ista, gaps_ista, c=cm(0), linestyle='--', label='gap PGD')
 # plt.semilogy(gaps_fista, c=cm(1), linestyle='--', label='gap APGD')
@@ -97,6 +118,9 @@ plt.semilogy(times_ista_lsbb, E_ista_lsbb - p_star, c=cm(5), label='primal subop
 # plt.semilogy(gaps_anderson, c=cm(3), linestyle='--', label='gap PGD Anderson')
 # plt.semilogy(times_ista_ls, gaps_ista_ls, c=cm(4), linestyle='--', label='gap PGD+LS')
 # plt.semilogy(times_ista_lsbb, gaps_ista_lsbb, c=cm(5), linestyle='--', label='gap PGD+LSBB')
+
 plt.legend()
+plt.title(f"{dataset}, reg = {reg}, q = {q}")
+
 plt.xlabel("Time (s)")
 plt.show(block=False)
--- code/slope/solvers/pgd.py
+++ code/slope/solvers/pgd.py
@@ -8,99 +8,102 @@ from slope.utils import dual_norm_slope, prox_slope
 
 
 def prox_grad(
-        X, y, alphas, fista=False, max_epochs=100, tol=1e-10, gap_freq=1,
-        anderson=False, line_search=False, barzilai=False, ls_type=1, verbose=True):
-    if anderson and fista:
-        raise ValueError("anderson=True cannot be combined with fista=True")
-    if barzilai and not line_search:
-        raise ValueError("Cannot use Barzilai rule without linesearch.")
+    X,
+    y,
+    alphas,
+    acceleration="none",
+    bb_step_size=False,
+    line_search=False,
+    gap_freq=1,
+    max_epochs=100,
+    tol=1e-10,
+    verbose=True,
+):
+    if acceleration not in ["none", "anderson", "fista"]:
+        raise ValueError(
+            "`acceleration` must be one of 'none', 'anderson', and 'fista'"
+        )
+
+    if bb_step_size and not line_search:
+        raise ValueError("cannot use Barzilai rule without line search")
+
+    # TODO: I think there are methods that make this possible, right?
+    if bb_step_size and acceleration == "fista":
+        raise ValueError("cannot use Barzilai rule with FISTA")
+
     n_samples, n_features = X.shape
     R = y.copy()
     w = np.zeros(n_features)
     theta = np.zeros(n_samples)
-    # FISTA parameters:
+
+    # FISTA parameters
     z = w.copy()
     t = 1
 
-    if anderson:
+    # BB parameters
+    gamma = 2.0
+    prev_bb = 1.0
+
+    if acceleration == "anderson":
         K = 5
         last_K_w = np.zeros([K + 1, n_features])
         U = np.zeros([K, n_features])
 
-    times = []
     time_start = timer()
-    times.append(timer() - time_start)
 
-    L = 1.0
+    L = 1
 
     if not line_search:
         if sparse.issparse(X):
             L = sparse.linalg.svds(X, k=1)[1][0] ** 2 / n_samples
         else:
-            L = norm(X, ord=2)**2 / n_samples
-    print("init L", L)
+            L = norm(X, ord=2) ** 2 / n_samples
+
     # Line search parameter
     eta = 2.0
 
+    times = []
+    times.append(timer() - time_start)
     E, gaps = [], []
-    E.append(norm(y)**2 / (2 * n_samples))
+    E.append(norm(y) ** 2 / (2 * n_samples))
     gaps.append(E[0])
     for it in range(max_epochs):
         R[:] = y - X @ z
-        if it > 0:
+        if it > 0 and bb_step_size:
             grad_old = grad
         grad = -(X.T @ R) / n_samples
 
         if line_search:
-            # print(f"iter {it}")
-            # Barzilai-Borwein rule:
-            # L = 1
-            if ls_type == 1:
-                L *= 0.9
-            elif ls_type == 2:
-                L = 1.0
-
-            if barzilai and it > 0:
+            if bb_step_size and it > 0:
                 delta_w = w - w_old
                 delta_grad = grad - grad_old
-                L = np.abs(delta_w @ delta_grad) / norm(delta_w) ** 2
-                # print("BB guess:", L)
-            f_old = norm(R) ** 2 / (2 * n_samples)
 
-            condition = 0
+                # BB step size safe-guarding
+                delta_w_grad_dot = np.dot(delta_w, delta_grad)
+                bb1 = (norm(delta_w) ** 2) / delta_w_grad_dot
+                bb2 = delta_w_grad_dot / (norm(delta_grad) ** 2)
+
+                bb = bb2 if bb1 < gamma * bb2 else bb1 - bb2 / gamma
+                L = 1.0 / bb if bb > 0 else 1.0 / prev_bb
+            else:
+                L *= 0.9
+
+            f_old = norm(R) ** 2 / (2 * n_samples)
 
             while True:
                 w_new = prox_slope(z - grad / L, alphas / L)
                 f = norm(X @ w_new - y) ** 2 / (2 * n_samples)
                 d = w_new - z
-                q = f_old + d @ grad + 0.5 * L * norm(d)**2
+                q = f_old + d @ grad + 0.5 * L * norm(d) ** 2
 
-                if ls_type == 3:
-                    if q >= f * (1 - 1e-12):
-                        if condition == 0:
-                            condition = 1
-                        elif condition == 2:
-                            break
-
-                        L *= 0.9
-                    else:
-                        if condition == 0:
-                            condition = 2
-                        elif condition == 1:
-                            break
-
-                        L *= eta
+                if f <= q:
+                    break
                 else:
-                    if q >= f * (1 - 1e-12):
-                        break
-                    else:
-                        L *= eta
-
-            # print("L taken:", L)
+                    L *= eta
         else:
             w_new = prox_slope(z - grad / L, alphas / L)
 
-        if anderson:
+        if acceleration == "anderson":
             # TODO multiple improvements possible here
             if it < K + 1:
                 last_K_w[it] = w_new
@@ -116,20 +119,22 @@ def prox_grad(
                 try:
                     coefs = np.linalg.solve(C, np.ones(K))
                     c = coefs / coefs.sum()
-                    w_acc = np.sum(last_K_w[:-1] * c[:, None],
-                                   axis=0)
-                    p_obj = norm(y - X @ w_new) ** 2 / (2 * n_samples) + \
-                        np.sum(alphas * np.sort(np.abs(w_new))[::-1])
-                    p_obj_acc = norm(y - X @ w_acc) ** 2 / (2 * n_samples) + \
-                        np.sum(alphas * np.sort(np.abs(w_acc))[::-1])
+                    w_acc = np.sum(last_K_w[:-1] * c[:, None], axis=0)
+                    p_obj = norm(y - X @ w_new) ** 2 / (2 * n_samples) + np.sum(
+                        alphas * np.sort(np.abs(w_new))[::-1]
+                    )
+                    p_obj_acc = norm(y - X @ w_acc) ** 2 / (2 * n_samples) + np.sum(
+                        alphas * np.sort(np.abs(w_acc))[::-1]
+                    )
                     if p_obj_acc < p_obj:
                         w_new = w_acc
                 except np.linalg.LinAlgError:
                     if verbose:
-                        print("----------Linalg error")
+                        print("linalg error, skipping anderson update")
         w_old = w
-        if fista:
-            t_new = (1 + np.sqrt(1 + 4 * t ** 2)) / 2
+
+        if acceleration == "fista":
+            t_new = (1 + np.sqrt(1 + 4 * t**2)) / 2
             z = w_new + (t - 1) / t_new * (w_new - w)
             w = w_new
             t = t_new
@@ -142,10 +147,10 @@ def prox_grad(
             theta = R / n_samples
             theta /= max(1, dual_norm_slope(X, theta, alphas))
 
-            dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / \
-                (2 * n_samples)
-            primal = norm(R) ** 2 / (2 * n_samples) + \
-                np.sum(alphas * np.sort(np.abs(w))[::-1])
+            dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / (2 * n_samples)
+            primal = norm(R) ** 2 / (2 * n_samples) + np.sum(
+                alphas * np.sort(np.abs(w))[::-1]
+            )
 
             E.append(primal)
             gap = primal - dual
@@ -156,4 +161,5 @@ def prox_grad(
                 print(f"Epoch: {it + 1}, loss: {primal}, gap: {gap:.2e}")
             if gap < tol:
                 break
+
     return w, np.array(E), np.array(gaps), np.array(times)

5d134e8a50aa681d5b0ceff21f09e4aa4f8e40a7
Johan Larsson <johanlarsson@outlook.com>
2022-06-03 09:31:11 +0200

    fix: actually assign prev_bb

--- code/slope/solvers/pgd.py
+++ code/slope/solvers/pgd.py
@@ -84,6 +84,7 @@ def prox_grad(
 
                 bb = bb2 if bb1 < gamma * bb2 else bb1 - bb2 / gamma
                 L = 1.0 / bb if bb > 0 else 1.0 / prev_bb
+                prev_bb = bb
             elif acceleration not in ["bb", "anderson"]:
                 L *= 0.9
 

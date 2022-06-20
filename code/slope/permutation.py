d4748a3f4492db21bb152b684ddac1ff45701a7d
Johan Larsson <johanlarsson@outlook.com>
2022-06-20 16:56:49 +0200

    chore: autoformat with black

--- code/slope/permutation.py
+++ code/slope/permutation.py
@@ -2,11 +2,8 @@
 Various permuation, ordering objects
 
 """
-from scipy import sparse
 import numpy as np
-
-
-
+from scipy import sparse
 
 
 def nonzero_sign(x):
@@ -18,6 +15,7 @@ def nonzero_sign(x):
         out[i] = s if s != 0 else 0
 
     return out
+
 
 def permutation_matrix(x):
     n = len(x)
@@ -32,42 +30,47 @@ def permutation_matrix(x):
 
     return sparse.csc_array(pi)
 
-#build the signedpermutation object
+
+# build the signedpermutation object
 def build_pi(x):
-    
+
     n = len(x)
-    pi_list  = np.empty((n,2),dtype=np.int64)
-    piT_list = np.empty((n,2),dtype=np.int64)
+    pi_list = np.empty((n, 2), dtype=np.int64)
+    piT_list = np.empty((n, 2), dtype=np.int64)
     signs = nonzero_sign(x)
     order = np.argsort(np.abs(x))[::-1]
-    
+
     for j, ord_j in enumerate(order):
         pi_list[j, 0] = ord_j
         pi_list[j, 1] = signs[ord_j]
-        piT_list[ord_j,0] = j
+        piT_list[ord_j, 0] = j
         piT_list[ord_j, 1] = signs[ord_j]
 
     return pi_list, piT_list
-    
+
+
 # multiplaction of signed permuation
 def pix(x, pi_list):
-    return(x[pi_list[:,0]]*pi_list[:,1])
+    return x[pi_list[:, 0]] * pi_list[:, 1]
+
 
 # inverse of the matrix B.T
 def BTinv(x):
-    return(np.cumsum(x))
+    return np.cumsum(x)
 
 
 # inverse of the matrix B
 def Binv(x):
-    return( np.cumsum(x[::-1])[::-1])
+    return np.cumsum(x[::-1])[::-1]
+
 
 def B(x):
     y = x.copy()
     y[:-1] -= x[1:]
-    return(y)
+    return y
 
-#Bt^-1B^-1
+
+# Bt^-1B^-1
 # returns (BBt) ^-1 B x
 def BBT_inv_B(x):
-    return( BTinv(x))
+    return BTinv(x)

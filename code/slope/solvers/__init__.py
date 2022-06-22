e92b059f8fc54c6a4f968adb5647222d6f4b7f10
Johan Larsson <johanlarsson@outlook.com>
2022-06-22 14:47:38 +0200

    fix: revert import change

--- code/slope/solvers/__init__.py
+++ code/slope/solvers/__init__.py
@@ -1,4 +1,6 @@
-from slope.solvers.pgd import prox_grad
 from slope.solvers.hybrid import hybrid_cd
 from slope.solvers.oracle import oracle_cd
+from slope.solvers.pgd import prox_grad
 from slope.solvers.newtalm import newt_alm
+
+__all__ = ["prox_grad", "hybrid_cd", "oracle_cd", "newt_alm"]

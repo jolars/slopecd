b41092755ae28b1052ea99d6ce9e447691a8512b
Johan Larsson <johanlarsson@outlook.com>
2022-06-22 14:54:19 +0200

    fix: reinsert newt_alm export

--- code/slope/solvers/__init__.py
+++ code/slope/solvers/__init__.py
@@ -1,7 +1,6 @@
-from slope.solvers.pgd import prox_grad
 from slope.solvers.hybrid import hybrid_cd
+from slope.solvers.pgd import prox_grad
 from slope.solvers.oracle import oracle_cd
+from slope.solvers.newtalm import newt_alm
 
-__all__ = ['prox_grad',
-           'hybrid_cd',
-           'oracle_cd']
+__all__ = ["prox_grad", "hybrid_cd", "oracle_cd", "newt_alm"]

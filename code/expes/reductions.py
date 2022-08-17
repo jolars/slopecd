849ae090a457b962d00764cc146a90291db10ba8
Johan Larsson <johanlarsson@outlook.com>
2022-08-17 11:44:00 +0200

    feat: update experiment

--- code/expes/reductions.py
+++ code/expes/reductions.py
@@ -7,11 +7,12 @@ from slope.data import get_data
 from slope.solvers import hybrid_cd
 from slope.utils import dual_norm_slope
 
-dataset = "bcTCGA"
-# dataset = "Scheetz2006"
+# dataset = "rcv1.binary"
+dataset = "Scheetz2006"
+# dataset = "bcTCGA"
 # dataset = "simulated"
 if dataset == "simulated":
-    X, y, _ = make_correlated_data(n_samples=10, n_features=100, random_state=10)
+    X, y, _ = make_correlated_data(n_samples=10, n_features=10, random_state=0)
     # X = csc_matrix(X)
 else:
     X, y = get_data(dataset)
@@ -28,11 +29,17 @@ alpha_max = dual_norm_slope(X, (y - fit_intercept * np.mean(y)) / len(y), alphas
 
 alphas = alpha_max * alphas_seq * reg
 
-max_epochs = 100_000
-max_time = 100
+max_epochs = 10000
+max_time = np.inf
 tol = 1e-6
 
-beta_cd_reduced, intercept_cd_reduced, primals_cd_reduced, gaps_cd_reduced, time_cd_reduced = hybrid_cd(
+(
+    beta_cd_reduced,
+    intercept_cd_reduced,
+    primals_cd_reduced,
+    gaps_cd_reduced,
+    time_cd_reduced,
+) = hybrid_cd(
     X,
     y,
     alphas,
@@ -41,33 +48,38 @@ beta_cd_reduced, intercept_cd_reduced, primals_cd_reduced, gaps_cd_reduced, time
     verbose=True,
     tol=tol,
     max_time=max_time,
-    use_reduced_X=True
+    use_reduced_X=True,
+    cluster_updates=True
 )
 
-# beta_cd, intercept_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
-#     X,
-#     y,
-#     alphas,
-#     fit_intercept=fit_intercept,
-#     max_epochs=max_epochs,
-#     verbose=True,
-#     tol=tol,
-#     max_time=max_time,
-#     use_reduced_X=False
-# )
+beta_cd, intercept_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
+    X,
+    y,
+    alphas,
+    fit_intercept=fit_intercept,
+    max_epochs=max_epochs,
+    verbose=True,
+    tol=tol,
+    max_time=max_time,
+    use_reduced_X=False,
+    cluster_updates=True
+)
 
+primals_star = np.min(np.hstack((primals_cd, np.array(primals_cd_reduced))))
 
-# # Time vs. duality gap
-# plt.clf()
+plt.clf()
+plt.semilogy(time_cd, primals_cd - primals_star, label="cd")
+plt.semilogy(time_cd_reduced, primals_cd_reduced - primals_star, label="cd_reduced")
 
 # plt.semilogy(time_cd, gaps_cd, label="cd")
-# plt.semilogy(time_cd_reduced, gaps_cd_reduced, label="cd_reduced")
-
-# # plt.semilogy(gaps_cd, label="cd")
-# # plt.semilogy(gaps_cd_reduced, label="cd_reduced")
-
-# plt.ylabel("duality gap")
+# plt.semilogy(time_cd_reduced, gaps_cd_reduced, label="cd_updates")
 # plt.xlabel("Time (s)")
-# plt.legend()
-# plt.title(dataset)
-# plt.show(block=False)
+
+# plt.semilogy(np.arange(len(gaps_cd))*10, gaps_cd, label="cd")
+# plt.semilogy(np.arange(len(gaps_cd_reduced))*10, gaps_cd_reduced, label="cd_reduced")
+# plt.xlabel("Epoch")
+
+plt.ylabel("suboptimality")
+plt.legend()
+plt.title(dataset)
+plt.show(block=False)

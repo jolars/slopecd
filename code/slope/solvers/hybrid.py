d4f67ba15a9f8e0482d4e21ab254c4dc68fa41f6
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-08-17 15:51:34 +0200

    feat: make cluster_updates=True the default (#36)

--- code/slope/solvers/hybrid.py
+++ code/slope/solvers/hybrid.py
@@ -6,11 +6,7 @@ from numpy.linalg import norm
 from scipy import sparse
 
 from slope.clusters import get_clusters, update_cluster
-from slope.utils import (
-    dual_norm_slope,
-    prox_slope,
-    slope_threshold
-)
+from slope.utils import dual_norm_slope, prox_slope, slope_threshold
 
 
 @njit
@@ -34,13 +30,13 @@ def block_cd_epoch(
             j += 1
             continue
 
-        cluster = cluster_indices[cluster_ptr[j]:cluster_ptr[j+1]]
+        cluster = cluster_indices[cluster_ptr[j] : cluster_ptr[j + 1]]
         sign_w = np.sign(w[cluster]) if c[j] != 0 else np.ones(len(cluster))
         sum_X = X[:, cluster] @ sign_w
         L_j = sum_X.T @ sum_X / n_samples
         c_old = abs(c[j])
         x = c_old + (sum_X.T @ R) / (L_j * n_samples)
-        beta_tilde, ind_new = slope_threshold(x, alphas/L_j, cluster_ptr, c, n_c, j)
+        beta_tilde, ind_new = slope_threshold(x, alphas / L_j, cluster_ptr, c, n_c, j)
 
         w[cluster] = beta_tilde * sign_w
         if c_old != beta_tilde:
@@ -72,7 +68,7 @@ def block_cd_epoch_sparse(
     c,
     n_c,
     cluster_updates,
-    update_zero_cluster
+    update_zero_cluster,
 ):
     n_samples = len(R)
 
@@ -82,14 +78,15 @@ def block_cd_epoch_sparse(
             j += 1
             continue
 
-        cluster = cluster_indices[cluster_ptr[j]:cluster_ptr[j+1]]
+        cluster = cluster_indices[cluster_ptr[j] : cluster_ptr[j + 1]]
         sign_w = np.sign(w[cluster]) if c[j] != 0 else np.ones(len(cluster))
         sum_X = compute_block_scalar_sparse(
-            X_data, X_indices, X_indptr, sign_w, cluster, n_samples)
+            X_data, X_indices, X_indptr, sign_w, cluster, n_samples
+        )
         L_j = sum_X.T @ sum_X / n_samples
         c_old = abs(c[j])
         x = c_old + (sum_X.T @ R) / (L_j * n_samples)
-        beta_tilde, ind_new = slope_threshold(x, alphas/L_j, cluster_ptr, c, n_c, j)
+        beta_tilde, ind_new = slope_threshold(x, alphas / L_j, cluster_ptr, c, n_c, j)
         w[cluster] = beta_tilde * sign_w
         if c_old != beta_tilde:
             R += (c_old - beta_tilde) * sum_X
@@ -108,11 +105,10 @@ def block_cd_epoch_sparse(
 
 
 @njit
-def compute_block_scalar_sparse(
-        X_data, X_indices, X_indptr, v, cluster, n_samples):
+def compute_block_scalar_sparse(X_data, X_indices, X_indptr, v, cluster, n_samples):
     scal = np.zeros(n_samples)
     for k, j in enumerate(cluster):
-        start, end = X_indptr[j:j+2]
+        start, end = X_indptr[j : j + 2]
         for ind in range(start, end):
             scal[X_indices[ind]] += v[k] * X_data[ind]
     return scal
@@ -123,7 +119,7 @@ def hybrid_cd(
     y,
     alphas,
     fit_intercept=True,
-    cluster_updates=False,
+    cluster_updates=True,
     update_zero_cluster=False,
     pgd_freq=5,
     gap_freq=10,
@@ -159,10 +155,10 @@ def hybrid_cd(
         else:
             spectral_norm = norm(X, ord=2)
 
-        L = spectral_norm ** 2 / n_samples
+        L = spectral_norm**2 / n_samples
 
     E, gaps = [], []
-    E.append(norm(y)**2 / (2 * n_samples))
+    E.append(norm(y) ** 2 / (2 * n_samples))
     gaps.append(E[0])
 
     for epoch in range(max_epochs):
@@ -187,7 +183,7 @@ def hybrid_cd(
                     c,
                     n_c,
                     cluster_updates,
-                    update_zero_cluster
+                    update_zero_cluster,
                 )
             else:
                 n_c = block_cd_epoch(
@@ -200,7 +196,7 @@ def hybrid_cd(
                     c,
                     n_c,
                     cluster_updates,
-                    update_zero_cluster
+                    update_zero_cluster,
                 )
 
             if fit_intercept:
@@ -213,10 +209,10 @@ def hybrid_cd(
         if epoch % gap_freq == 0 or times_up:
             theta = R / n_samples
             theta /= max(1, dual_norm_slope(X, theta, alphas))
-            dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / \
-                (2 * n_samples)
-            primal = norm(R) ** 2 / (2 * n_samples) + \
-                np.sum(alphas * np.sort(np.abs(w))[::-1])
+            dual = (norm(y) ** 2 - norm(y - theta * n_samples) ** 2) / (2 * n_samples)
+            primal = norm(R) ** 2 / (2 * n_samples) + np.sum(
+                alphas * np.sort(np.abs(w))[::-1]
+            )
 
             E.append(primal)
             gap = primal - dual

af1fb97e97f8c96e8c48c7a9283e553c923249f3
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-08-23 18:54:45 +0200

    feat: cache and re-use reductions and Hessian terms (#39)
    * feat: cache reductions
    
    * refactor: avoid initializing memory
    
    * refactor: introduce c_perm to keep track of order
    
    * fix: fix perm updates for reductions
    
    * refactor: small improvements to clusters
    
    * fix: make tests for clusters work again
    
    * chore: clean up
    
    * refactor: remove unused argument
    
    * feat: update experiment
    
    * fix bug in oracle for sparse matrix
    
    * chore: remove unused c_old
    
    * reduced X for sparse matrices
    
    * fix: uncomment function to make tests pass
    
    * refactor: remove reductions for the sparse case
    
    * refactor: improve sparse operations, parallelize
    
    * refactor: use dict instead
    
    * refactor: rename rows to more intuitive inds
    
    * chore: add a note about possible speedups
    
    * chore: update experiments
    
    Co-authored-by: Klopfe <quentin.klopfenstein@u-bourgogne.fr>

--- code/expes/reductions.py
+++ code/expes/reductions.py
@@ -8,23 +8,20 @@ from slope.solvers import hybrid_cd, oracle_cd
 from slope.utils import dual_norm_slope
 
 dataset = "rcv1.binary"
-# dataset = "news20"
-# dataset = "Scheetz2006"
-# dataset = "YearPredictionMSD"
-# dataset = "bcTCGA"
 # dataset = "Rhee2006"
+# dataset = "bcTCGA"
 # dataset = "simulated"
 if dataset == "simulated":
     X, y, _ = make_correlated_data(n_samples=10, n_features=10, random_state=0)
     # X = csc_matrix(X)
 else:
     X, y = get_data(dataset)
-
+# X = X.todense()
 fit_intercept = False
 
 randnorm = stats.norm(loc=0, scale=1)
 q = 0.1
-reg = 0.05
+reg = 0.01
 
 alphas_seq = randnorm.ppf(1 - np.arange(1, X.shape[1] + 1) * q / (2 * X.shape[1]))
 
@@ -34,25 +31,7 @@ alphas = alpha_max * alphas_seq * reg
 
 max_epochs = 10000
 max_time = np.inf
-tol = 1e-6
-
-(
-    beta_cd_reduced,
-    intercept_cd_reduced,
-    primals_cd_reduced,
-    gaps_cd_reduced,
-    time_cd_reduced,
-) = hybrid_cd(
-    X,
-    y,
-    alphas,
-    fit_intercept=fit_intercept,
-    max_epochs=max_epochs,
-    verbose=True,
-    tol=tol,
-    max_time=max_time,
-    use_reduced_X=True,
-)
+tol = 1e-4
 
 beta_cd, intercept_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
     X,
@@ -63,22 +42,36 @@ beta_cd, intercept_cd, primals_cd, gaps_cd, time_cd = hybrid_cd(
     verbose=True,
     tol=tol,
     max_time=max_time,
-    use_reduced_X=False,
+    cluster_updates=True,
 )
-primals_star = np.min(np.hstack((np.array(primals_cd), np.array(primals_cd_reduced))))
+
+beta_oracle, intercept_oracle, primals_oracle, gaps_oracle, time_oracle = oracle_cd(
+    X,
+    y,
+    alphas,
+    fit_intercept=fit_intercept,
+    max_epochs=max_epochs,
+    verbose=True,
+    tol=tol,
+    max_time=max_time,
+    w_star=beta_cd,
+)
+
+primals_star = np.min(np.hstack((primals_cd, np.array(primals_oracle))))
 
 plt.clf()
+plt.semilogy(time_cd, primals_cd - primals_star, label="cd_hybrid")
+plt.semilogy(time_oracle, primals_oracle - primals_star, label="cd_oracle")
 
-plt.semilogy(time_cd, primals_cd - primals_star, label="cd")
-plt.semilogy(time_cd_reduced, primals_cd_reduced - primals_star, label="cd_reduced")
+# plt.semilogy(time_cd, gaps_cd, label="cd")
+# plt.semilogy(time_cd_reduced, gaps_cd_reduced, label="cd_updates")
+# plt.xlabel("Time (s)")
 
-plt.xlabel("Time (s)")
-
-# plt.semilogy(np.arange(len(gaps_cd_reduced))*10, gaps_cd_reduced, label="cd")
 # plt.semilogy(np.arange(len(gaps_cd))*10, gaps_cd, label="cd")
+# plt.semilogy(np.arange(len(gaps_cd_reduced))*10, gaps_cd_reduced, label="cd_reduced")
 # plt.xlabel("Epoch")
 
 plt.ylabel("suboptimality")
 plt.legend()
-plt.title(f"{dataset}, reg: {reg}, q: {q}, tol: {tol}")
+plt.title(dataset)
 plt.show(block=False)

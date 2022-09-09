b72fe725e15feea99ceeb70a9795a4f6c47b72c8
Johan Larsson <johanlarsson@outlook.com>
2022-09-09 22:03:08 +0200

    feat: update 2D illustration

--- code/expes/illustration_2D_slope.py
+++ code/expes/illustration_2D_slope.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.collections import LineCollection
 from numpy.linalg import norm
 from numpy.random import default_rng
 from scipy import stats
@@ -11,14 +12,8 @@ from slope.utils import ConvergenceMonitor, dual_norm_slope, prox_slope, slope_t
 dir_results = "../../figures/"
 savefig = False
 
-# def abline(slope, intercept):
-#     axes = plt.gca()
-#     x_vals = np.array(axes.get_xlim())
-#     y_vals = intercept + slope * x_vals
-#     plt.plot(x_vals, y_vals, "--", color="grey")
 
-
-def cd(X, y, alphas, max_iter, beta0):
+def cd(X, y, alphas, max_iter, beta0, verbose=False):
     n_samples = X.shape[0]
     beta = beta0.copy()
     R = y - X @ beta
@@ -27,14 +22,14 @@ def cd(X, y, alphas, max_iter, beta0):
     beta2s = []
 
     monitor = ConvergenceMonitor(
-        X, y, alphas, 1e-12, 1, np.inf, True, intercept_column=False
+        X, y, alphas, 1e-12, 1, np.inf, verbose, intercept_column=False
     )
-
     c, c_ptr, c_ind, c_perm, n_clusters = get_clusters(beta)
 
     for it in range(max_iter):
         j = 0
         while j < n_clusters:
+            c, c_ptr, c_ind, c_perm, n_clusters = get_clusters(beta)
             beta1s.append(beta[0])
             beta2s.append(beta[1])
 
@@ -53,7 +48,7 @@ def cd(X, y, alphas, max_iter, beta0):
                 R += (c_old - beta_tilde) * sum_X
 
             ind_old = j
-            _ = update_cluster(
+            n_clusters = update_cluster(
                 c,
                 c_ptr,
                 c_ind,
@@ -78,41 +73,48 @@ def cd(X, y, alphas, max_iter, beta0):
     return beta1s, beta2s
 
 
-def hybrid_cd(X, y, alphas, max_iter, beta0):
+def hybrid_cd(X, y, alphas, max_iter, beta0, verbose=False):
     n_samples = X.shape[0]
     beta = beta0
 
     R = y - X @ beta
 
-    beta1s = []
-    beta2s = []
+    betas = []
+    beta_pgd = []
+    beta_cd = []
+
+    beta_tmp = []
 
     L = norm(X, ord=2) ** 2 / n_samples
 
     monitor = ConvergenceMonitor(
-        X, y, alphas, 1e-12, 1, np.inf, True, intercept_column=False
+        X, y, alphas, 1e-12, 1, np.inf, verbose, intercept_column=False
     )
 
     c, c_ptr, c_ind, c_perm, n_clusters = get_clusters(beta)
 
     for it in range(max_iter):
         if it % 5 == 0:
-            beta1s.append(beta[0])
-            beta2s.append(beta[1])
+            betas.append(beta.copy())
+            if it > 0:
+                beta_cd.append(beta_tmp.copy())
+                beta_tmp.clear()
+            beta_prev = beta.copy()
             beta = prox_slope(beta + X.T @ R / (L * n_samples), alphas)
             R = y - X @ beta
+            beta_pgd.append(np.array([beta_prev, beta.copy()]))
+            beta_tmp.append(beta.copy())
+            betas.append(beta.copy())
         else:
             j = 0
 
             while j < n_clusters:
-                beta1s.append(beta[0])
-                beta2s.append(beta[1])
 
                 c, c_ptr, c_ind, c_perm, n_clusters = get_clusters(beta)
 
-                if c[j] == 0:
-                    j += 1
-                    continue
+                # if c[j] == 0:
+                #     j += 1
+                #     continue
 
                 cluster = c_ind[c_ptr[j] : c_ptr[j + 1]]
                 sign_w = np.sign(beta[cluster]) if c[j] != 0 else np.ones(len(cluster))
@@ -146,14 +148,20 @@ def hybrid_cd(X, y, alphas, max_iter, beta0):
                     use_reduced_X=False,
                 )
 
+                beta_tmp.append(beta.copy())
+
                 j += 1
+            betas.append(beta.copy())
         converged = monitor.check_convergence(beta, 0.0, it)
         if converged:
+            if it % 5 != 0:
+                beta_cd.append(beta_tmp.copy())
             break
-    return beta1s, beta2s
+
+    return np.vstack(betas), beta_pgd, beta_cd
 
 
-def pgd(X, y, alphas, max_iter, beta0):
+def pgd(X, y, alphas, max_iter, beta0, verbose=False):
 
     beta = beta0
     R = y - X @ beta
@@ -164,7 +172,7 @@ def pgd(X, y, alphas, max_iter, beta0):
     L = norm(X, ord=2) ** 2 / n_samples
 
     monitor = ConvergenceMonitor(
-        X, y, alphas, 1e-12, 1, np.inf, True, intercept_column=False
+        X, y, alphas, 1e-12, 1, np.inf, verbose, intercept_column=False
     )
 
     for it in range(max_iter):
@@ -181,12 +189,11 @@ def pgd(X, y, alphas, max_iter, beta0):
 
 n_samples = 10
 
-rng = default_rng(4)
+rng = default_rng(3)
 
-# X = np.random.rand(n_samples, 2)
 cov = [[1, 0.85], [0.85, 1]]
 X = rng.multivariate_normal(size=n_samples, mean=[0.0, 0.0], cov=cov)
-beta_true = np.array([0.25, -0.25])
+beta_true = np.array([0.2, -0.2])
 
 y = X @ beta_true
 
@@ -200,21 +207,28 @@ alphas = alpha_max * alphas_seq * reg
 
 beta0 = np.array([-0.8, 0.3])  # try to make beta slope have 1 cluster
 
-n_it = 20
+n_it = 10
+
+betas_hybrid, betas_hybrid_pgd, betas_hybrid_cd = hybrid_cd(X, y, alphas, n_it, beta0)
 
 beta1s_cd, beta2s_cd = cd(X, y, alphas, n_it, beta0)
-beta1s_hybrid, beta2s_hybrid = hybrid_cd(X, y, alphas, n_it, beta0)
 beta1s_pgd, beta2s_pgd = pgd(X, y, alphas, n_it, beta0)
 betas = (
     (beta1s_cd, beta2s_cd),
-    (beta1s_hybrid, beta2s_hybrid),
+    (betas_hybrid[:, 0], betas_hybrid[:, 1]),
     (beta1s_pgd, beta2s_pgd),
 )
 beta_star, primals_star, gaps_star, _, _ = prox_grad(
-    X, y, alphas, max_epochs=1000, verbose=True, fit_intercept=False
+    X, y, alphas, max_epochs=1000, verbose=False, fit_intercept=False
 )
-beta1 = np.linspace(-1.0, 1.0, 40)
-beta2 = np.linspace(-1.0, 1.0, 40)
+
+beta1 = np.linspace(xmin, xmax, 40)
+beta2 = np.linspace(ymin, ymax, 40)
+
+xmin = np.min([np.min(betas[i][0]) for i in range(3)]) - 0.1
+xmax = np.max([np.max(betas[i][0]) for i in range(3)]) + 0.1
+ymin = np.min([np.min(betas[i][1]) for i in range(3)]) - 0.1
+ymax = np.max([np.max(betas[i][1]) for i in range(3)]) + 0.1
 
 z = np.ndarray((40, 40))
 
@@ -229,32 +243,48 @@ for i in range(40):
         z[j][i] = gap
 
 
-labels = ["Naive CD", "Hybrid", "PGD"]
+labels = ["CD", "Hybrid", "PGD"]
 
 plt.close("all")
 
-fig, axarr = plt.subplots(1, 3, sharey=True, figsize=[7, 2.5], constrained_layout=True)
+fig, axarr = plt.subplots(
+    1, 3, sharey=True, sharex=True, figsize=[7, 2.5], constrained_layout=True
+)
+
 for i in range(3):
     ax = axarr[i]
-    ax.contour(beta1, beta2, z, levels=30)
+    ax.contour(beta1, beta2, z, levels=30, colors="darkgrey")
     x_vals = np.array(ax.get_xlim())
     y_vals = -x_vals
-    ax.plot(x_vals, y_vals, "--", color="grey")
+    ax.plot(x_vals, y_vals, ":", color="grey")
     x_vals = np.array(ax.get_xlim())
     y_vals = x_vals
-    ax.plot(x_vals, y_vals, "--", color="grey")
+    ax.plot(x_vals, y_vals, ":", color="grey")
     ax.set_title(labels[i])
     ax.plot(
         beta_star[0], beta_star[1], color="darkorange", marker="x", markersize=10, mew=2
     )
+    linestyle = "" if i == 1 else "-"
     ax.plot(
         betas[i][0],
         betas[i][1],
-        marker="o",
-        color="cornflowerblue",
+        marker=".",
+        color="black",
         label=labels[i],
-        markersize=4,
+        linestyle=linestyle,
+        markersize=8,
     )
+
+    if i == 1:
+        # Plot separate linestyles for CD and hybrid steps
+        lc_cd = LineCollection(betas_hybrid_cd, color="black")
+        lc_pgd = LineCollection(betas_hybrid_pgd, linestyle="--", color="black")
+        ax.add_collection(lc_cd)
+        ax.add_collection(lc_pgd)
+
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+
     ax.set_aspect("equal")
     ax.set_xlabel(r"$\beta_1$")
     if i == 0:

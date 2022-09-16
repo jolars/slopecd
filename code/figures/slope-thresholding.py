b7eec501015348ca92c9dffbdbca78cbc346207f
Johan Larsson <johanlarsson@outlook.com>
2022-09-16 14:41:06 +0200

    feat: update thresholding op figure

--- code/figures/slope-thresholding.py
+++ code/figures/slope-thresholding.py
@@ -7,14 +7,29 @@ from slope.utils import dual_norm_slope
 
 
 def get_clusters(w):
-    unique, indices, counts = np.unique(np.abs(w),
-                                        return_inverse=True,
-                                        return_counts=True)
+    unique, indices, counts = np.unique(
+        np.abs(w), return_inverse=True, return_counts=True
+    )
 
     clusters = [[] for _ in range(len(unique))]
     for i in range(len(indices)):
         clusters[indices[i]].append(i)
     return clusters[::-1], counts[::-1], indices[::-1], unique[::-1]
+
+
+def ann(ax, label, coord):
+    ax.annotate(
+        label,
+        coord,
+        xycoords="axes fraction",
+        verticalalignment="bottom",
+        horizontalalignment="left",
+        rotation=60,
+    )
+
+
+def identity(x):
+    return x
 
 
 np.random.seed(10)
@@ -88,9 +103,7 @@ res = []
 zerosum = np.sum(lambdas[::-1][range(2)])
 
 for a in a_list:
-    k = np.where(
-        np.logical_and(np.sign(a) * a <= l_sums,
-                       np.sign(a) * a >= r_sums))[0]
+    k = np.where(np.logical_and(np.sign(a) * a <= l_sums, np.sign(a) * a >= r_sums))[0]
 
     if np.abs(a) < zerosum:
         res.extend([0.0])
@@ -103,21 +116,73 @@ for a in a_list:
         ll = len(l_sums) - bisect_left(l_sums[::-1], np.abs(a))
         res.extend([np.sign(a) * (np.abs(a) - sums[ll])])
 
-plt.rcParams['text.usetex'] = True
+plt.rcParams["text.usetex"] = True
 
-fig, ax = plt.subplots(figsize=(4.2, 2.8))
+plt.close("all")
+
+fig, ax = plt.subplots(figsize=(6.2, 4.8), constrained_layout=True)
+
 ax.hlines(0, xmin=min(a_list), xmax=max(a_list), color="lightgrey")
-ax.vlines(np.hstack((-l_sums, -r_sums, l_sums, r_sums, sums[-1], -sums[-1])),
-          ymin=min(res),
-          ymax=max(res),
-          linestyles="dotted",
-          color="black")
-ax.plot(a_list, res, '-', color="black")
-ax.set_ylabel("$T(v,\\lambda)$")
-ax.set_xlabel("$v$")
+
+lambda_sums = np.sort(
+    np.hstack((-l_sums, -r_sums, l_sums, r_sums, sums[-1], -sums[-1]))
+)
+
+ax.vlines(
+    lambda_sums,
+    ymin=min(res),
+    ymax=max(res),
+    linestyles="dotted",
+)
+
+xlim = ax.get_xlim()
+
+xy = np.stack(
+    (
+        ((lambda_sums - xlim[0]) / (xlim[1] - xlim[0])),
+        np.repeat([1.01], len(lambda_sums)),
+    ),
+    axis=1,
+)
+
+x2_labs = (
+    r"\(-\omega c^{\setminus k}_1 - \sum_{j \in C(c^{\setminus k}_1 + \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_1 + \varepsilon_c}}\)",
+    r"\(-\omega c^{\setminus k}_1 - \sum_{j \in C(c^{\setminus k}_1 - \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_1 - \varepsilon_c}}\)",
+    r"\(-\omega c^{\setminus k}_2 - \sum_{j \in C(c^{\setminus k}_2 + \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_2 + \varepsilon_c}}\)",
+    r"\(-\omega c^{\setminus k}_2 - \sum_{j \in C(c^{\setminus k}_2 - \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_2 - \varepsilon_c}}\)",
+    r"-\(\sum_{j \in C(0)} \lambda_{(j)^-_{\varepsilon_c}}\)",
+    r"\(\sum_{j \in C(0)} \lambda_{(j)^-_{\varepsilon_c}}\)",
+    r"\(\omega c^{\setminus k}_2 + \sum_{j \in C(c^{\setminus k}_2 - \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_2 - \varepsilon_c}}\)",
+    r"\(\omega c^{\setminus k}_2 + \sum_{j \in C(c^{\setminus k}_2 + \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_2 + \varepsilon_c}}\)",
+    r"\(\omega c^{\setminus k}_1 + \sum_{j \in C(c^{\setminus k}_1 - \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_1 - \varepsilon_c}}\)",
+    r"\(\omega c^{\setminus k}_1 + \sum_{j \in C(c^{\setminus k}_1 + \varepsilon_c)} \lambda_{(j)^-_{c^{\setminus k}_1 + \varepsilon_c}}\)",
+)
+
+ax.plot(a_list, res, "-", color="black")
+ax.set_ylabel(r"\(T(\gamma, \omega; c, \lambda)\)")
+ax.set_xlabel(r"\(\gamma\)")
+
+ax2_x = ax.secondary_xaxis("top")
+ax2_x.set_xticks(
+    lambda_sums,
+    labels=x2_labs,
+    rotation=60,
+    verticalalignment="bottom",
+    horizontalalignment="left",
+)
+
+y2_labs = (
+    r"$-c^{\setminus k}_1$",
+    r"$-c^{\setminus k}_2$",
+    r"$c^{\setminus k}_2$",
+    r"$c^{\setminus k}_1$",
+)
+
+y2_vals = np.sort(np.hstack((-np.delete(c, 1), np.delete(c, 1))))
+
+ax2_y = ax.secondary_yaxis("right")
+ax2_y.set_yticks(y2_vals, y2_labs)
+
 # plt.show(block=False)
 
-plt.tight_layout()
-plt.savefig("../figures/slope-thresholding.pdf",
-            bbox_inches="tight",
-            pad_inches=0.01)
+plt.savefig("../figures/slope-thresholding.pdf", bbox_inches="tight", pad_inches=0.01)

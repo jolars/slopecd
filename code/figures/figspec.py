85d3d4b13f6a746bb42713f244f577f702fa4d0d
Johan Larsson <johanlarsson@outlook.com>
2022-10-14 10:57:13 +0200

    refactor: reorg figures and modify pgdfreq fig
    Use a figures folder within tex folder instead and create a makefile for
    creating submissions

--- code/figures/figspec.py
+++ code/figures/figspec.py
@@ -5,10 +5,9 @@ from pyprojroot import here
 FULL_WIDTH = 6.75
 HALF_WIDTH = 3.25
 
-
 def fig_path(x):
     code_dir = Path(here())
     root_dir = code_dir.parent
-    fig_dir = root_dir / "figures"
+    fig_dir = root_dir / "tex" / "figures"
 
     return fig_dir / x
